7e03a370f2939db0eace0e0d0e76d27c3021b471
Moddingus <106451891+Moddingus@users.noreply.github.com>
2023-05-18 18:59:15 +0800

    all of them

---
+++ TheDepths/NPCs/Chasme/ChasmeHandLeft.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheDepths.NPCs.Chasme;
+
+public class ChasmeHandLeft : ChasmeHand
+{
+	public override bool IsLoadingEnabled(Mod mod) => true;
+
+	protected override Vector2 BaseOffset => new(x: 114, y: 95);
+
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
+		NPC.width = 160;
+		NPC.height = 166;
+		shootOffset = 100;
+	}
+}
+public class ChasmeHandLeftExpert : ChasmeHandLeft
+{
+	public override bool IsLoadingEnabled(Mod mod) => false;
+
+	protected override Vector2 BaseOffset => new(x: 114, y: 95);
+
+    public override void SetDefaults()
+	{
+        NPC.width = 160;
+        NPC.height = 166;
+        shootOffset = 150;
+	}
+}
---
+++ TheDepths/TheDepths.cs
@@ -0,0 +1,32 @@
+﻿using log4net.Repository.Hierarchy;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Dyes;
+using Terraria.GameContent.UI;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.UI;
+using TheDepths;
+namespace TheDepths
+{
+	public class TheDepths : Mod
+	{
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                //GameShaders.Misc["TheDepths:ChasmeDeath"] = new MiscShaderData(new Ref<Effect>((Effect)ModContent.Request<Effect>("TheDepths/Shaders/ChasmeDeathEffect", ReLogic.Content.AssetRequestMode.ImmediateLoad)), "ChasmeAnimation").UseImage0("Images/Misc/Perlin");
+            }
+        }
+    }
+}
+
